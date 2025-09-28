e1a8ba370e2c1a4d039ba33a5b1d76ecd79da3c3
Your Name <you@example.com>
2025-09-28 11:25:49 +0300

    navigation animation to home view

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -2,9 +2,9 @@ import 'dart:async';
 
 import 'package:bookly/core/resources/color_manager.dart';
 import 'package:bookly/core/resources/fonts_manager.dart';
-import 'package:bookly/core/resources/routes_manager.dart';
 import 'package:bookly/core/resources/strings_manager.dart';
 import 'package:bookly/core/resources/styles_manager.dart';
+import 'package:bookly/features/home/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
 
 class SplashViewBody extends StatefulWidget {
@@ -14,25 +14,60 @@ class SplashViewBody extends StatefulWidget {
   State<SplashViewBody> createState() => _SplashViewBodyState();
 }
 
-class _SplashViewBodyState extends State<SplashViewBody> {
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
   Timer? _timer;
+  late AnimationController _controller;
+  late Animation<Offset> _slidingAnimation;
   void delay() {
     _timer = Timer(Duration(seconds: 3), _goNext);
   }
 
   void _goNext() {
-    Navigator.pushReplacementNamed(context, Routes.homeRoute);
+    // Navigator.pushReplacementNamed(context, Routes.homeRoute);
+    Navigator.push(
+      context,
+      PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) => HomeView(),
+        transitionDuration: Duration(milliseconds: 200),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          const begin = Offset(1.0, 0.0); // يبدأ من يمين الشاشة
+          const end = Offset.zero; // ينتهي في مكانه الطبيعي
+          const curve = Curves.easeInOut;
+          final tween = Tween(
+            begin: begin,
+            end: end,
+          ).chain(CurveTween(curve: curve));
+          final offsetAnimation = animation.drive(tween);
+          return SlideTransition(position: offsetAnimation, child: child);
+        },
+      ),
+    );
+  }
+
+  void animatedText() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+    _slidingAnimation = Tween<Offset>(
+      begin: Offset(0, 1),
+      end: Offset(0, 0),
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.linear));
+    _controller.forward();
   }
 
   @override
   void initState() {
     delay();
+    animatedText();
     super.initState();
   }
 
   @override
   void dispose() {
     _timer?.cancel();
+    _controller.dispose();
     super.dispose();
   }
 
@@ -57,10 +92,13 @@ class _SplashViewBodyState extends State<SplashViewBody> {
               ),
               duration: Duration(seconds: 2),
               builder: (BuildContext context, Color? value, Widget? child) {
-                return Text(
-                  AppStrings.readFreeBooks,
-                  textAlign: TextAlign.center,
-                  style: getBoldStyle(color: value!, fontSize: FontSize.s22),
+                return SlideTransition(
+                  position: _slidingAnimation,
+                  child: Text(
+                    AppStrings.readFreeBooks,
+                    textAlign: TextAlign.center,
+                    style: getBoldStyle(color: value!, fontSize: FontSize.s22),
+                  ),
                 );
               },
             ),
