74e185f0ae4dcf15fd7b678750bbb3d7be5fe50c
Your Name <you@example.com>
2025-10-05 04:47:00 +0300

    locate service to make singleton for homerepoimpl

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,7 +1,12 @@
 import 'package:bookly/core/resources/routes_manager.dart';
 import 'package:bookly/core/resources/theme_manager.dart';
+import 'package:bookly/features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly/features/home/presentation/view_models/physics_books_cubit/physics_books_cubit.dart';
+import 'package:bookly/features/home/presentation/view_models/programming_books_cubit/programming_books_cubit.dart';
 import 'package:bookly/features/splash/presentation/views/splash_view.dart';
+import 'package:bookly/services/locator_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Bookly extends StatelessWidget {
   const Bookly._internal();
@@ -10,11 +15,21 @@ class Bookly extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashView(),
-      theme: getApplicationTheme(),
-      onGenerateRoute: RouteGenerator.getRoute,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => ProgrammingBooksCubit(getIt.get<HomeRepoImpl>()),
+        ),
+        BlocProvider(
+          create: (context) => PhysicsBooksCubit(getIt.get<HomeRepoImpl>()),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SplashView(),
+        theme: getApplicationTheme(),
+        onGenerateRoute: RouteGenerator.getRoute,
+      ),
     );
   }
 }
---
+++ lib/services/locator_service.dart
@@ -0,0 +1,11 @@
+import 'package:bookly/features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly/services/api_service.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+void setup() {
+  getIt.registerSingleton<HomeRepoImpl>(HomeRepoImpl(ApiService(Dio())));
+
+  // Alternatively you could write it if you don't like global variables
+}
